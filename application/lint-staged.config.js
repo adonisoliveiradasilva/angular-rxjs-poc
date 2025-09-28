1d9b8ef25fd502a3c91719b1f354b170c26d3e38
adonis oliveira <adonis.silva@grupolpj.com>
2025-09-28 12:33:32 -0300

    teste

--- application/lint-staged.config.js
+++ application/lint-staged.config.js
@@ -1,15 +1,6 @@
 module.exports = {
-  '*.{ts,js}': [
-    'npx eslint --fix',
-    'npx prettier --write'
-  ],
-  '*.html': [
-    'npx prettier --write'
-  ],
-  '*.scss': [
-    'npx prettier --write'
-  ],
-  '*.json': [
-    'npx prettier --write'
-  ]
+  "*.{ts,js}": ["npx eslint --fix", "npx prettier --write"],
+  "*.html": ["npx prettier --write"],
+  "*.scss": ["npx prettier --write"],
+  "*.json": ["npx prettier --write"]
 };
